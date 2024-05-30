29aa5849381ea390022b140e119fed513f5d4542
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2024-05-30 21:06:43 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Warrior warrior = new Warrior("eva", 100);
+        Mage mage = new Mage("eda", 100);
+        warrior.attack(mage);
+
+    }
+}
+
+
