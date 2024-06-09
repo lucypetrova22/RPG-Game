0581f1358e1dbf620ba4c0e95383683a0b7663b3
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2024-06-09 17:02:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Warrior warrior = new Warrior("eva", 100);
-        Mage mage = new Mage("eda", 100);
-        warrior.attack(mage);
+        Scanner scanner = new Scanner(System.in);
+        Warrior warrior = new Warrior("Lucy", 100);
+        Mage mage = new Mage("Eda", 100);
+        Rogue rogue = new Rogue("Thief", 100);
 
+        do {
+            System.out.println("A new fight begins!");
+
+
+            warrior.attack(mage);
+            mage.castSpell(warrior);
+            rogue.attack(mage);
+
+            warrior.levelUp();
+            mage.levelUp();
+            rogue.levelUp();
+
+            System.out.print("do you want another fight? (yes/no): ");
+        } while (scanner.nextLine().equalsIgnoreCase("yes"));
+        scanner.close();
+        System.out.println("Game over. Thank you for playing!");
     }
 }
-
-
