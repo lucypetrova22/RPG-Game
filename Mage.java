fc2c2506fe290956527d7bf2859591b3289fbce5
Lucy Petrova <164482879+lucypetrova22@users.noreply.github.com>
2024-06-09 17:02:51 +0300

    Update Mage.java

--- Mage.java
+++ Mage.java
@@ -8,9 +8,22 @@ class Mage extends Character {
         this.spellPower = 15;
     }
 
-
+    
     public void castSpell(Character target) {
-        target.setHealth(target.getHealth() - 15);
-        System.out.println(getName() + " casts a spell on " + target.getName() + " and deals 15 damage. " + target.getName() + "'s new health is " + target.getHealth());
+        if (getMana() >= 15) {
+            target.setHealth(target.getHealth() - 15);
+            setMana(getMana() - 15);
+            System.out.println(getName() + " casts a spell on " + target.getName() + " and deals 15 damage. " + target.getName() + "'s new health is " + target.getHealth() + ". " + getName() + "'s remaining mana is " + getMana());
+        } else {
+            System.out.println(getName() + " does not have enough mana to cast a spell.");
+        }
+    }
+
+    
+    public void levelUp() {
+        super.levelUp();
+        this.intelligence += 2;
+        this.spellPower += 3;
+        System.out.println(getName() + "'s intelligence increases to " + this.intelligence + " and spell power increases to " + this.spellPower);
     }
 }
